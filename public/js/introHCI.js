23d3fe8684ef4d90f3433a06f03774a530930e87
JasonJiang <jiangzidong01@gmail.com>
2014-02-13 23:42:11 +0000

    put together lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,9 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	console.log("/project/"+idNumber);
+	var projID = "/project/"+idNumber;
+	$.get(projID, addProject);
 }
 
 /*
@@ -35,4 +38,34 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	
+	var randomPalette = $(this).closest('.palette').attr('colors');
+	//console.log(randomPalette);
+	//var idNumber = paletteID.substr('palette'.length);
+	var color = "/palette:"+randomPalette;
+	$.get(color, rdmColor);
+}
+
+function addProject(result)
+{
+	console.log(result);
+	//console.log('#'+result['id']);
+	var projectHTML = '<a href="#" class="thumbnail">' + '<img src="'+result['image']+'" class="img">' + '<p>' + result['title'] + '</p>' + '<p><small>' + result['date'] + '</small></p></a>';
+	$("#project-container").html(projectHTML);
+	$("#project-description").html(result['summary']);
+	//Below is the line using multiple selections in Ajax
+	$("#project"+result['id']+" .details").html(projectHTML);
+
+}
+
+function rdmColor(result)
+{
+	//console.log(result['colors']);
+	var array = result['colors'];
+	var colors = array['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
